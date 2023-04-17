f0a5b1f8eb28c1a315a6000d1c12a55dce3e16d8
Luis Manjarrez carbajal <darenhein@Hotmail.com>
2023-04-16 18:22:18 -0600

    v3

--- src/Menu.java
+++ src/Menu.java
@@ -9,7 +9,7 @@ public class Menu extends JFrame{
     private int largo = 600; 
     private JPanel panel;
     private JLabel e,e1,e2,e3,e4; 
-    private JTextField c,c1,c2; 
+    private JTextField c,c1,c2,c3; 
     private JButton b,b1,b2; 
 
     public Menu(){
@@ -25,8 +25,8 @@ public class Menu extends JFrame{
         panel.setLayout(null);
         this.getContentPane().add(panel); 
         eti();
-       // campos(); 
-        //botones(); 
+        campos(); 
+        botones(); 
     }
     public void eti(){
         e = new JLabel("CAJERTO ATM"); 
@@ -43,6 +43,37 @@ public class Menu extends JFrame{
         e3.setBounds(100,200,120,20); 
         e3.setFont(new Font("arial",Font.BOLD,18)); 
         panel.add(e3); 
+
+        e4 = new JLabel("Deposito"); 
+        e4.setBounds(100, 280, 120, 20);
+        e4.setFont(new Font("arial",Font.BOLD,18)); 
+        panel.add(e4); 
+    }
+    public void campos(){
+        c = new JTextField("0"); 
+        c.setBounds(350, 120, 120, 25);
+        c.setEditable(false);
+        panel.add(c); 
+
+        c2 = new JTextField(); 
+        c2.setBounds(350, 200, 120, 25);
+        panel.add(c2); 
+
+        c3 = new JTextField(); 
+        c3.setBounds(350, 280, 120, 25);
+        panel.add(c3); 
+
+
+    }
+
+    public void botones(){
+        b = new JButton("Siguiente");
+        b.setBounds(350, 450, 120, 25);
+        panel.add(b);  
+
+        b2 = new JButton("Salis");
+        b2.setBounds(150, 450, 120, 25);
+        panel.add(b2);  
     }
 
 }
