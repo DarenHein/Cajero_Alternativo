55013ad9eb329f6cc0e3e116fa56ec9f58e56005
Luis Manjarrez carbajal <darenhein@Hotmail.com>
2023-04-19 00:17:43 -0600

    v4

--- src/Menu.java
+++ src/Menu.java
@@ -45,12 +45,12 @@ public class Menu extends JFrame{
         e2.setFont(new Font("arial",Font.BOLD,18)); 
         panel.add(e2); 
 
-        e3 = new JLabel("Retiro"); 
+        e3 = new JLabel("Deposito"); 
         e3.setBounds(100,200,120,20); 
         e3.setFont(new Font("arial",Font.BOLD,18)); 
         panel.add(e3); 
 
-        e4 = new JLabel("Deposito"); 
+        e4 = new JLabel("Retiro"); 
         e4.setBounds(100, 280, 120, 20);
         e4.setFont(new Font("arial",Font.BOLD,18)); 
         panel.add(e4); 
@@ -62,11 +62,11 @@ public class Menu extends JFrame{
         panel.add(c); 
 
         c2 = new JTextField("0"); 
-        c2.setBounds(350, 200, 120, 25);// retiro
+        c2.setBounds(350, 200, 120, 25);// deposito
         panel.add(c2); 
 
         c3 = new JTextField("0"); 
-        c3.setBounds(350, 280, 120, 25);// deposito
+        c3.setBounds(350, 280, 120, 25);// retiro 
         panel.add(c3); 
 
 
@@ -82,9 +82,9 @@ public class Menu extends JFrame{
              
                 try {
                    String csaldo = c.getText(); 
-                   String cretiro = c2.getText(); 
+                   String cdeposito = c2.getText(); 
                    int num = Integer.parseInt(csaldo); 
-                   int num2 = Integer.parseInt(cretiro); 
+                   int num2 = Integer.parseInt(cdeposito); 
                    int resultado = num + num2; 
                    System.out.println(resultado);
                    String resultado2 = String.valueOf(resultado); 
@@ -95,12 +95,6 @@ public class Menu extends JFrame{
                     // TODO: handle exception
                     JOptionPane.showMessageDialog(null,"Caracter invalido intenta otra vez ");
                 }
-
-            
-             
-             
-             
-
             }
         }); 
 
@@ -125,6 +119,26 @@ public class Menu extends JFrame{
 
         b3 = new JButton("Retiro"); 
         b3.setBounds(400, 450, 120, 25);
+        b3.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               try {
+                String csaldo = c.getText(); 
+               String cretiro = c3.getText();
+               int num = Integer.parseInt(csaldo); 
+               int num2 = Integer.parseInt(cretiro); 
+               int resultado = num - num2 ; 
+               String resultado2 = String.valueOf(resultado); 
+               c.setText(resultado2); 
+               c3.setText("0");
+               } catch (Exception e45) {
+                // TODO: handle exception
+                JOptionPane.showMessageDialog(null,"Opcion invalida");
+               }
+            }
+            
+        });
         panel.add(b3);
     }
 
